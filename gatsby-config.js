b72acd057fbdcc0d53b8a25ae062628259111815
Nevil Sandaradura <npsand@utu.fi>
2021-06-11 19:57:25 +0300

    Finished Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
+  
   siteMetadata: {
     title: "My Gatsby Site",
+    
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+  "gatsby-plugin-gatsby-cloud",
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",{
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+  },
+]
 };
