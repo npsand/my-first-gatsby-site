9b02a6a509ae5ade7973fe6ff54e8789fd19e393
Nevil Sandaradura <npsand@utu.fi>
2021-06-11 19:33:19 +0300

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/gallery.js
@@ -0,0 +1,11 @@
+import Frame from '../components/frame'
+import React from 'react'
+
+const GalleryPage = () => {
+  return (
+    <Frame>
+      <p>This will be passed in as children</p>
+    </Frame>
+  )
+}
+export default GalleryPage
